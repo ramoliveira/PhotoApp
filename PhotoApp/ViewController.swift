ae50ca8839c62c11eefa737194be62a4a38e7e40
Ramón Almeida <ramon.almeida@southsystem.com.br>
2023-03-28 15:07:25 -0300

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Ramón Dias de Oliveira Almeida on 28/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
